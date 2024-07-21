54c05c7fcb9aca6ba3bbab5b23dec77e8ccfefe8
Krishna0172 <krishkrishna2710@gmail.com>
2024-07-21 19:11:49 +0530

    preview commit

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -9,11 +9,12 @@ const Portfolio = () => {
   return (
     <div className="content">
       <div className="header">
-        <img className="logo" src={richard} alt="Logo"></img>
+        <img className="logo" src={richard} alt="Logo" />
         <p className="logo-moto">GIVE POWER TO THE PIXEL</p>
         <div className="profile">
           <h1>
-            I<span className="hypen">'</span>M SHAIJU RICHARD
+            I<span className="hypen">'</span>M  
+            SHAIJU RICHARD
             <span className="hypen_2">.</span>
           </h1>
           <p>UI & GRAPHICS DESIGNER</p>
@@ -21,7 +22,7 @@ const Portfolio = () => {
       </div>
       <div className="center">
         <div className="box">
-          <img className="logo" src={richard} alt="Logo"></img>
+          <img className="logo" src={richard} alt="Logo" />
           <p className="logo-moto">GIVE POWER TO THE PIXEL</p>
           <p className="bio">
             Peoples find expert designers to complete their work and I am an
@@ -45,21 +46,21 @@ const Portfolio = () => {
 
         <div className="bars">
           <div className="bar">
-            <p>PHOTOSHOP</p>
-            <p>FIGMA</p>
-            <p>ILLUSTRATOR</p>
-            <p>COREL DRAW</p>
-            <p>INDESIGN</p>
-            <p>CANVA</p>
-            <p>CAPCUT PC</p>
+            <p className="skills">PHOTOSHOP</p>
+            <p className="skills">FIGMA</p>
+            <p className="skills">ILLUSTRATOR</p>
+            <p className="skills">COREL DRAW</p>
+            <p className="skills">INDESIGN</p>
+            <p className="skills">CANVA</p>
+            <p className="skills">CAPCUT PC</p>
           </div>
           <div className="bar">
-            <p className="bar1 barline"></p>
-            <p className="bar2 barline"></p>
-            <p className="bar3 barline"></p>
-            <p className="bar4 barline"></p>
-            <p className="bar5 barline"></p>
-            <p className="bar6 barline"></p>
+            <p className="bar1"></p>
+            <p className="bar2"></p>
+            <p className="bar3"></p>
+            <p className="bar4"></p>
+            <p className="bar5"></p>
+            <p className="bar6"></p>
             <p className="bar7"></p>
           </div>
           <div className="bar">
@@ -73,7 +74,6 @@ const Portfolio = () => {
           </div>
         </div>
 
-
         <h1>02 PORTFOLIO</h1>
         <h3>MY LATEST WORKS </h3>
         <div className="image-container">
@@ -83,49 +83,63 @@ const Portfolio = () => {
             <img src={images3} alt="Image 3" />
           </div>
         </div>
+      <div className="educations">
         <h1>03 EDUCATION</h1>
         <div className="education_1">
-          <p class="dot">
+          <p className="dot">
             <p className="dot_line"></p>
           </p>
-          <p>2015-2016</p>
-          <p>SSLC</p>
-          <p>Sacred heart matriculation school,</p>
-          <p>Neyveli-607802.</p>
+          <div className="education1">
+            <p className="year">2015-2016</p>
+            <p>SSLC</p>
+            <p>Sacred heart matriculation school,</p>
+            <p>Neyveli-607802.</p>
+          </div>
         </div>
         <div className="education_2">
-          <p class="dot"></p>
-          <p>2017-2018</p>
-          <p>HSC</p>
-          <p>Vetri Vikaas matric hr sec school,</p>
-          <p>Rasipuram-641652.</p>
+          <p className="dot"></p>
+          <div className="education2">
+            <p className="year">2017-2018</p>
+            <p>HSC</p>
+            <p>Vetri Vikaas matric hr sec school,</p>
+            <p>Rasipuram-641652.</p>
+          </div>
         </div>
         <div className="education_3">
-          <p class="dot"></p>
-          <p>2018-2021</p>
-          <p>BCOM COMPUTER APPLICATION</p>
-          <p>Muthayammal college of arts & science,</p>
-          <p>Rasipuram-637408.</p>
+          <p className="dot"></p>
+          <div className="education3">
+            <p className="year">2018-2021</p>
+            <p>BCOM COMPUTER APPLICATION</p>
+            <p>Muthayammal college of arts & science,</p>
+            <p>Rasipuram-637408.</p>
+          </div>
         </div>
         <div className="education_4">
-          <p class="dot"></p>
-          <p>2021-2023</p>
-          <p>MSC VISUAL COMMUNICATION</p>
-          <p>Hindustan college of arts & science,</p>
-          <p>coimbatore-641028.</p>
-        </div>
-        <div className="education_5">
-          <p class="dot"></p>
-          <p>2021-2023</p>
-          <p>MSC VISUAL COMMUNICATION</p>
-          <p>Hindustan college of arts & science,</p>
-          <p>coimbatore-641028.</p>
-          <p class="dot"></p>
+          <p className="dot"></p>
+          <div className="education4">
+            <p className="year">2021-2023</p>
+            <p>MSC VISUAL COMMUNICATION</p>
+            <p>Hindustan college of arts & science,</p>
+            <p>Coimbatore-641028.</p>
+          </div>
         </div>
 
-        <img className="logo_2" src={richard} alt="Logo"></img>
-        <p>Contact</p>
-        <p>Mail id: shaijurichard@gmail.com</p>
+        <div className="education_5">
+          <p className="dot"></p>
+          <div className="education5">
+            <p className="year">2023-2024</p>
+            <p>MASTER IN GRAPHICS DESIGN</p>
+            <p>Adoro Institute of Multimedia</p>
+            <p>Coimbatore - 641012.</p>
+          </div>
+          <p className="dot"></p>
+        </div>
+      </div>
+
+
+        <img className="logo_2" src={richard} alt="Logo" />
+        <p className="contact">Contact</p>
+        <p className="contact">Mail id: shaijurichard@gmail.com</p>
       </div>
     </div>
   );
